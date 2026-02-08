a5d245e3e5a0db388759745235f0c5b6755bba85
Przemek Malirz <p.malirz@gmail.com>
2026-02-08 18:25:53 +0100

    update storybook deps

--- frontend/shared-ui/vite.config.js
+++ frontend/shared-ui/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 const deps = pkg.dependencies;
 
